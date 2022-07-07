c68fd2ef7b31bc4bf9f9be12c06cf8ef2b2d3fe6
Alex Betancur <alexbetbu@gmail.com>
2022-07-06 23:48:57 -0500

    Creating random number list

--- src/ej3/App.java
+++ src/ej3/App.java
@@ -1,7 +1,28 @@
 package ej3;
 
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("_");
+        System.out.println("Random Numbers List:");
+
+        ArrayList<Integer> listNumber = new ArrayList<Integer>();
+
+
+        for(int i = 0; i < 10; i++) {
+            double data = Math.random()*100;
+            int number = (int)data;
+            listNumber.add(i, number);
+        }
+
+        for(int i = 0; i < listNumber.size(); i++) {
+            System.out.print(listNumber.get(i) + " ");
+        }
+
+        System.out.println("\n" + "To order the list");
+        System.out.println("Press 1 to order by bubble algorithm method");
+        System.out.println("Press 2 to order by quick sort method");
+
+
     }
 }
